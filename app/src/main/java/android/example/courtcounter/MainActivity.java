f83798c5ebc2a12199eb90fc122dfdf637e2c77e
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-04-23 21:15:20 -0800

    added TeamB funtions which add properly

--- app/src/main/java/android/example/courtcounter/MainActivity.java
+++ app/src/main/java/android/example/courtcounter/MainActivity.java
@@ -9,12 +9,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
     }
 
     /**
@@ -38,5 +38,28 @@ public class MainActivity extends AppCompatActivity {
     public void onePointTeamA (View view) {
         scoreTeamA +=1;
         displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void threePointsTeamB(View view) {
+        scoreTeamB +=3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void twoPointsTeamB(View view) {
+        scoreTeamB +=2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void onePointTeamB (View view) {
+        scoreTeamB +=1;
+        displayForTeamB(scoreTeamB);
     }
 }
